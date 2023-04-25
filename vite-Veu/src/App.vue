d259fc8a3ceb718a88595afd8ee3166c0425d65f
applestven <applestven@gmail.com.com>
2023-04-25 13:55:31 +0800

    first pull

---
+++ vite-Veu/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  测试成功！！！！ 
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  再次确定测试持续集成的能力
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ vite-Veu/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>hello  gitAction</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
