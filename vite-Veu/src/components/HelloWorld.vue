343740bf6fc824ecb5594ef185835fa5f4f7b3ed
applestven <applestven@gmail.com.com>
2023-04-25 15:00:52 +0800

    ce

--- vite-Veu/src/components/HelloWorld.vue
+++ vite-Veu/src/components/HelloWorld.vue
@@ -120,92 +120,64 @@ export default {
       this.waitingForOperand = true;
     }
   },
-  watch: {
-    style(newStyle) {
-      console.log("newStyle",newStyle);
-      if (newStyle === 'style1') {
-        import("../assets/css/style1.scss")
-      } else if (newStyle === 'style2') {
-        import("../assets/css/style2.scss")
-      }
-    }
-  },
-  created(){
-    if(this.style == 'style1' ) {
-      import("../assets/css/style1.scss")
-    }else{
-      
-    }
-  }  
 
 };
 </script>
 
-<style lang="scss">
-.content {
-  &.style1 {
-    background-color: red;
-    color: white;
-    @import "example1";
-  }
+<style lang="css">
 
-  &.style2 {
-    background-color: blue;
-    color: white;
-  }
+
+.calculator {
+  width: 350px;
+  margin: auto;
+  background-color: #555;
+  border-radius: 5px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
+  padding: 10px;
 }
 
-// .calculator {
-//   width: 350px;
-//   margin: auto;
-//   background-color: #555;
-//   border-radius: 5px;
-//   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
-//   padding: 10px;
-// }
+.display {
+  background-color: #fff;
+  color: #333;
+  font-size: 36px;
+  text-align: right;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
 
-// .display {
-//   background-color: #fff;
-//   color: #333;
-//   font-size: 36px;
-//   text-align: right;
-//   padding: 10px;
-//   margin-bottom: 10px;
-//   border-radius: 5px;
-// }
+.key-row {
+  display: flex;
+}
 
-// .key-row {
-//   display: flex;
-// }
+.key {
+  width: 70px;
+  height: 70px;
+  font-size: 24px;
+  color: #fff;
+  background-color: #999;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
 
-// .key {
-//   width: 70px;
-//   height: 70px;
-//   font-size: 24px;
-//   color: #fff;
-//   background-color: #999;
-//   margin: 5px;
-//   border: none;
-//   border-radius: 5px;
-//   cursor: pointer;
-// }
+.key.operation {
+  background-color: #ff8c00;
+}
 
-// .key.operation {
-//   background-color: #ff8c00;
-// }
+.key.equals {
+  background-color: #3db3f7;
+}
 
-// .key.equals {
-//   background-color: #3db3f7;
-// }
+.key:focus {
+  outline: none;
+}
 
-// .key:focus {
-//   outline: none;
-// }
-
-// .key:active {
-//   transform: translateY(3px);
-//   box-shadow: none;
-// }
+.key:active {
+  transform: translateY(3px);
+  box-shadow: none;
+}
 
 
 </style>
